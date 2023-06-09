7f4fe8da08ea87b4222339d972fd6dd7f6b3188a
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-06-09 18:16:53 +0530

    update

--- Practice/index.js
+++ Practice/index.js
@@ -1,7 +1,47 @@
 
-function addNumbers() {
-    let num1 = parseInt(document.getElementById("num1").value);
-    let num2 = parseInt(document.getElementById("num2").value);
-    let sum = num1 + num2;
-    document.getElementById("result").innerHTML = "Sum: " + sum;
+/*
+setTimeout(() => {console.log(1);
+  setTimeout(() => {console.log(2);
+      setTimeout(() => {console.log(3);
+          setTimeout(() => {console.log(4);
+              setTimeout(() => {console.log(5);
+                  setTimeout(() => {console.log(6);
+                      setTimeout(() => {
+                          console.log(7);
+                      }, 7000);
+                  }, 6000);
+              }, 5000);
+          }, 4000);
+      }, 3000);
+  }, 2000);
+}, 1000);
+*/
+
+/*
+function printNumberWithDelay(number)
+ {
+  console.log(number);
+  if (number < 7) {
+    setTimeout(function() {
+      printNumberWithDelay(number + 1);
+    }, number * 1000);
   }
+}*/
+
+
+function isPrime(num) {
+  return new Promise(function(resolve, reject) {
+    if (num <= 1) {
+      reject("Error: Number is not prime.");
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        reject("Error: Number is not prime.");
+      }
+    }
+    resolve("Success: Number is prime.");
+  });
+}
+isPrime(15)
+  .then(function(result) {console.log(result);})
+  .catch(function(error) {console.log(error);});
